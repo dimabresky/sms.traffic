c1136bfd7ab5b9f44d8fd384817bb0ce41e448b3
Дмитрий <dimabresky@gmail.com>
2026-05-11 07:38:51 +0300

    chore: remove deprecated files and clean up module structure

--- lang/ru/install/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$MESS['SMS_TRAFFIC_MODULE_NAME'] = 'SMS Traffic (SmartDelivery)';
-$MESS['SMS_TRAFFIC_MODULE_DESC'] = 'Отправка SMS через SmartDelivery API (smstraffic.by) для модуля «Служба сообщений».';
